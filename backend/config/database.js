d7c0f8a4877f1a9e4bbbb816c405850145f4ee25
iachintyatiwari <achintyatiwari0@gmail.com>
2024-10-16 00:18:42 +0530

    intial commit

---
+++ backend/config/database.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+exports.connectDB = () => {
+    mongoose.connect(process.env.MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology:true,
+    })
+    .then(() => console.log("DB Connected Successfully"))
+    .catch( (error) => {
+        console.log("DB Connection Failed");
+        console.error(error);
+        process.exit(1);
+    } )
+ };
+
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://iachintya:iachintya@db.dvcwg.mongodb.net/?retryWrites=true&w=majority&appName=DB";
+
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// exports.connectDB = () => {
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+// }
